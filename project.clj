de8dc8072c6e11f5c3dba2a6ca96a3a82fc8a7da
Thomas Järvstrand <tjarvstrand@gmail.com>
2015-04-13 18:03:36 +0200

    Fix version and url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject rbac "0.1.0-SNAPSHOT"
+(defproject rbac "0.1.0"
   :description "Role Based Access Control"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/tjarvstrand/rbac"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
